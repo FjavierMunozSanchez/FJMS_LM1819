eea747f145e53b2e67683bbb3a9b8daafd0fc304
Alumno <javicecomerce@gmail.com>
2019-02-11 14:02:06 +0100

    EJERCICIO5

--- PHP/Ejercicio5/tablamul.php
+++ PHP/Ejercicio5/tablamul.php
@@ -1,4 +1,4 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
@@ -10,18 +10,80 @@
 </head>
 <body>
 
-<div>
-<<?php
 
-for($tabla=1; $tabla<=10; $tabla++) {
- echo "<th>Tabla del $tabla </th>";
- 
- while ($i <= 10) {
-    echo "$tabla x $i = ". ($tabla*$i) . "<br/>";
+<?php
+
+echo "1. Desarrolle un ejercicio que muestre las tablas de multiplicar";
+
+echo "</br>";
+
+$num1=1;
+while($num1<=10)
+{
+echo "<table style='border: 1px solid black; float: left'>";
+echo "<tr align=center><td colspan=5>Tabla de multiplicar del ".$num1.":</td></tr>";
+$num2=1;
+while($num2<=10)
+{
+$mult=$num1*$num2;
+echo "<tr align=center><td>".$num1."</td><td> X </td><td>".$num2."</td><td> = </td><td>".$mult."</td></tr>";
+$num2++;
 }
+
+$num1++;
 }
+
+echo "</table>";
+
+echo "<div style='clear: both'>";
+
+echo "</br>";
+
+echo "2. Mostramos los números de los días del 1 a la fecha actual. Ejemplo: si hoy es 11 debe mostrar los números del 1 al 11.";
+
+$dias= date ("d");
+$dia=11;
+for ($dias=1; $dias < 11; $dias++) { 
+    print "<p>Día " . $dias . " ya ha pasado </p></br>";
+}
+
+print "Hoy es " . $dia . " de febrero de 2019";
+
+echo "</div>";
+
+echo "</br>";
+
+echo "3. Dado un vector de números positivos desordenado, recórralo y muestre los 3 números mayores.";
+
+echo "</br>";
+
+echo "<div style='clear: both'>";
+
+echo "Estos son los 3 números más grandes del vector (100, 150, 10, 30, 50, 300, 1000, 85, 99, 45):";
+
+echo "</br>";
+
+$numeros=array(100, 150, 10, 30, 50, 300, 1000, 85, 99, 45);   
+$resultado = array(); 
+$pos = 0;
+$resultado[$pos]=$numeros[$pos];   
+
+
+for($n=0;$n<count($numeros);$n++) {           
+
+  if($resultado[$pos]>$numeros[$n])      
+    $pos++;        
+
+  $resultado[$pos] = $numeros[$n]; 
+     
+}  
+
+for($n1=0;$n1<3;$n1++) echo $resultado[$n1]."<br>"; 
+
+echo "</div>";
+
 ?>
-</div>
+
 
     
 </body>
